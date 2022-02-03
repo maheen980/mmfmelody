209e4d82b56c536ac10813df0e440baa9caac1fb
maheen980 <97508318+maheen980@users.noreply.github.com>
2022-02-03 22:01:36 +0500

    Update settings.py

--- djBoilerplate/settings.py
+++ djBoilerplate/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    # 'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.facebook',
     # 'allauth.socialaccount.providers.github',
      'allauth.socialaccount.providers.google',
 
